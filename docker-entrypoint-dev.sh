e60fbb1bdf93b045932bc38cb5966f67fdf6ff37
Lucas Mirloup <97196064+lucasmirloup@users.noreply.github.com>
2025-07-23 16:33:08 +0200

    fix(entrypoint): dev: remove entrypoint
    https://www.wrike.com/open.htm?id=1699325547

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	php bin/console -V
-
-	echo 'PHP app ready!'
-fi
-
 exec docker-php-entrypoint "$@"
